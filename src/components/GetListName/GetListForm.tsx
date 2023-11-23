cb0b28bc8c4ffcdf023f0e4602771b59e1c4ab84
Nurzat Taalaibekova <tnurzat91@gmail.com>
2023-11-23 13:04:15 +0300

    preloader loading

--- src/components/GetListName/GetListForm.tsx
+++ src/components/GetListName/GetListForm.tsx
@@ -6,6 +6,7 @@ import CountriesInfoItem from '../CountriesInfo/CountriesInfoItem';
 const GetListForm = () => {
   const [countries, setCountries] = useState<NameListProps[]>([]);
   const [selectedCountryInfo, setSelectedCountryInfo] = useState<CountryInfo | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,8 +17,11 @@ const GetListForm = () => {
         }
         const data: NameListProps[] = await response.json();
         setCountries(data);
+
+        setLoading(false);
       } catch (error) {
         console.error('Error:', error);
+        setLoading(false);
       }
     };
 
@@ -40,18 +44,24 @@ const GetListForm = () => {
   return (
     <div className="container d-flex my-3 shadow-sm p-3 mb-5 bg-body-tertiary rounded">
       <div className="col-4 ms-3 overflow-y-auto" style={{maxHeight: "600px", fontSize: "18px", cursor: "pointer"}}>
-        {countries.map((country, index) => (
-          <div key={index}>
-            <GetListName
-              key={country.id}
-              name={country.name}
-              onClick={() => {
-                setSelectedCountryInfo(null);
-                clickNameCountry(country.name);
-              }}
-            />
+        {loading ? (
+          <div className="spinner-border text-primary" role="status">
+            <span className="visually-hidden">Loading...</span>
           </div>
-        ))}
+        ) : (
+          countries.map((country, index) => (
+            <div key={index}>
+              <GetListName
+                key={country.id}
+                name={country.name}
+                onClick={() => {
+                  setSelectedCountryInfo(null);
+                  clickNameCountry(country.name);
+                }}
+              />
+            </div>
+          ))
+        )}
       </div>
       <div className="col-8 ps-3">
         <CountriesInfoItem countryInfo={selectedCountryInfo} />
